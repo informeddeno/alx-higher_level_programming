a0e9244c800f2d7a4d58601d33344596f0c78006
informeddeno <informeddeno@gmail.com>
2023-09-11 11:17:01 -0700

    pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,14 +5,9 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-
-
-
 class Rectangle(BaseGeometry):
 
     """Represent a rectangle using BaseGeometry."""
-
-
 
     def __init__(self, width, height):
 
