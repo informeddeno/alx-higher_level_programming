f98a6d7502688ec7a956d5b7c026b7f126fd9e5a
informeddeno <informeddeno@gmail.com>
2023-09-11 11:21:58 -0700

    pycodestyle

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,22 +3,15 @@
 """Defines a class MyInt that inherits from int."""
 
 
-
-
-
 class MyInt(int):
 
     """Invert int operators == and !=."""
-
-
 
     def __eq__(self, value):
 
         """Override == opeartor with != behavior."""
 
         return self.real != value
-
-
 
     def __ne__(self, value):
 
