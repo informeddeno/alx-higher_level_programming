f5bb1c66770871101fcdc0699c4f55138e53aacb
informeddeno <informeddeno@gmail.com>
2023-11-30 13:58:38 -0800

    0x10-python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 # shellcheck disable=SC2046
-if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi;
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
